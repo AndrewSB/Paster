3a5cf232c3d413926f7aa2d42472b4f11a5ecfbb
Andrew Breckenridge <asbreckenridge@me.com>
2014-08-17 10:10:15 -0400

    init

---
+++ Paster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Paster
+//
+//  Created by Andrew Breckenridge on 8/17/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
