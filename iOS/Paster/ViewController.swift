04d60ea8552c2105922cf4954dac6e93a7e337a7
AndrewSB <asbreckenridge@me.com>
2014-11-03 16:07:02 -0800

    add iOS directory

---
+++ iOS/Paster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Paster
+//
+//  Created by Andrew Breckenridge on 8/17/14.
+//  Copyright (c) 2014 asb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println(UIPasteboard.generalPasteboard().pasteboardTypes())
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
